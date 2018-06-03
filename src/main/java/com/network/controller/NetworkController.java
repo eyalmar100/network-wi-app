44794bc9de99b63f7682f0b28760e0f77d9502af
eyal markovich <eyal.markovich@softimize.co>
2018-06-03 08:20:12 +0300

    controller

--- src/main/java/com/network/controller/NetworkController.java
+++ src/main/java/com/network/controller/NetworkController.java
@@ -21,123 +21,131 @@ import com.network.entity.Network;
 import com.network.service.DeviceService;
 import com.network.service.NetworkService;
 
+
+
 @RestController
-@RequestMapping("wifi-tracking/api")
+@RequestMapping("wifi-tracking/api/")
 public class NetworkController {
 
-    final static Logger logger = Logger.getLogger(NetworkController.class);
+	final static Logger logger = Logger.getLogger(NetworkController.class);
+	
+	@Autowired
+	private NetworkService networkService;
 
-    @Autowired
-    private NetworkService networkService;
+	@Autowired
+	private DeviceService deviceDervice; 
 
-    @Autowired
-    private DeviceService deviceDervice;
 
-    @GetMapping("/network")
-    public Network getById(@RequestParam("id") Long id) {
-        logger.debug(String.format("%s", "resuest for network get by id-starting "));
-        Network net = networkService.getNetworkById(id);
+	@GetMapping("/network")
+	public Network getById(@RequestParam("id") Long id) {
+		logger.debug(String.format("%s","resuest for network get by id-starting "));
+		Network net=networkService.getNetworkById(id);
 
-        if (net != null) {
-            List<Device> devices = net.getDevices();
-            net.setDevices(devices);
-        }
-        logger.debug(String.format("%s", "resuest for connect-ok "));
-        return net;
-    }
+		if(net != null){
+		List<Device>devices=net.getDevices(); 
+		net.setDevices(devices);
+		}
+		logger.debug(String.format("%s","resuest for connect-ok "));
+		return net;
+	}
 
-    @PutMapping("/network/connect")
-    public ResponseEntity<Void> connect(@RequestBody String payload) {
+	@PutMapping("/network/connect")
+ 	public ResponseEntity<Void> connect(@RequestBody String payload){
 
-        logger.debug(String.format("%s", "resuest for connect a new device "));
-        JSONObject json;
-        try {
-            json = new JSONObject(payload);
+		logger.debug(String.format("%s","resuest for connect a new device "));
+		JSONObject json;
+		try {
+			json = new JSONObject(payload);
 
-            String deviceId = json.getString("device_id");
-            String deviceName = json.getString("device_name");
-            long networkId = json.getLong("network_id");
-            String auth = json.getString("auth");
-            Network network = networkService.getNetworkById(networkId);
-            Device device = new Device(deviceId, deviceName);
-            if (network == null) {
-                logger.debug(String.format("%s", "creating new network object "));
-                network = new Network(networkId, auth);
-                handleDevices(network, device);
-                network.setAvgThroughput();
-                networkService.saveNewNetwork(network);
-            } else {
-                logger.debug(String.format("%s", "found exiting network in db "));
-                handleDevices(network, device);
-                networkService.update(network);
-            }
-            logger.debug(String.format("%s", "resuest for connect a new device -ok"));
-            return new ResponseEntity<Void>(HttpStatus.CREATED);
-        } catch (JSONException e) {
+			String deviceId=json.getString("device_id"); 
+			String deviceName=json.getString("device_name");
+			long networkId=json.getLong("network_id");
+			String auth=json.getString("auth");
+			Network network=networkService.getNetworkById(networkId);
+			Device device=new Device(deviceId,deviceName);
+			if(network==null){
+				logger.debug(String.format("%s","creating new network object "));
+				network=new Network(networkId,auth);
+				handleDevices(network, device);
+				network.setAvgThroughput();
+				networkService.saveNewNetwork(network);
+			}
+			else{
+				logger.debug(String.format("%s","found exiting network in db "));
+				handleDevices(network, device);
+				networkService.update(network);
+			}
+			logger.debug(String.format("%s","resuest for connect a new device -ok"));
+			return new ResponseEntity<Void>(HttpStatus.CREATED);	
+		} catch (JSONException e) {
+			 
+			logger.error( e.getMessage());
+		}
+		return null;
+	}
 
-            logger.error(e.getMessage());
-        }
-        return null;
-    }
+	@PostMapping("/network/report")
+	public ResponseEntity<Void> report(@RequestBody String payload){
+		logger.debug(String.format("%s","creating new report   "));
+		float throughput;
+		try {
+			JSONObject json=new JSONObject(payload);
+			String deviceId=json.getString("device_id"); 
+			long networkId=json.getLong("network_id");
+ 			throughput = Float.valueOf(json.getString("throughput"));
+ 			networkService.report(deviceId,networkId,throughput);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		} catch (NumberFormatException e) {
+			
+			logger.error( e.getMessage());
+		
+		} catch (JSONException e) {
+		 
+			logger.error( e.getMessage());
+		}
+		logger.warn(String.format("%s","some problme with reporting  "));
+		return null;
+	}
 
-    @PostMapping("/network/report")
-    public ResponseEntity<Void> report(@RequestBody String payload) {
-        logger.debug(String.format("%s", "creating new report   "));
-        float throughput;
-        try {
-            JSONObject json = new JSONObject(payload);
-            String deviceId = json.getString("device_id");
-            long networkId = json.getLong("network_id");
-            throughput = Float.valueOf(json.getString("throughput"));
-            networkService.report(deviceId, networkId, throughput);
-            return new ResponseEntity<Void>(HttpStatus.OK);
-        } catch (NumberFormatException e) {
+	private void handleDevices(Network network, Device device) {
+		List<Device> listDevices=network.getDevices();
+		listDevices.add(device);
+		device.setNetwork(network);
+		network.setAvgThroughput();
+	}
 
-            logger.error(e.getMessage());
-
-        } catch (JSONException e) {
-
-            logger.error(e.getMessage());
-        }
-        logger.warn(String.format("%s", "some problme with reporting  "));
-        return null;
-    }
-
-    private void handleDevices(Network network, Device device) {
-        List<Device> listDevices = network.getDevices();
-        listDevices.add(device);
-        device.setNetwork(network);
-        network.setAvgThroughput();
-    }
-
-    /*
+	/*
 	 * Just Suggestion.. not implemented fully , but seems to be required ..
-     */
-    @PostMapping("/network/disconnect")
-    public ResponseEntity<Void> disconnect(@RequestBody String payload) {
+	 */
+	
+	@PostMapping("/network/disconnect")
+	public ResponseEntity<Void> disconnect(@RequestBody String payload){
+		  
+		   JSONObject json;
+		 
+			json = new JSONObject(payload);
+            // read params from json to get deviceId, and network id
+			// get the network obj and device obj , something like :
+			String deviceId=json.getString("device_id"); 
+			String deviceName=json.getString("device_name");
+			long networkId=json.getLong("network_id");
+			String auth=json.getString("auth");
+			Network network=networkService.getNetworkById(networkId);
+			Device device=new Device(deviceId,deviceName);
+			List<Device>devices=network.getDevices();
+			int size=devices.size();
+			for(int i=0;i<size;++i){
+				if(device.getId().equals(deviceId)){
+					devices.remove(i);
+					break;
+				}
+			}
+		//set avg throughput of network..
+		// remove device from db ..	
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
 
-        JSONObject json;
 
-        json = new JSONObject(payload);
-        // read params from json to get deviceId, and network id
-        // get the network obj and device obj , something like :
-        String deviceId = json.getString("device_id");
-        String deviceName = json.getString("device_name");
-        long networkId = json.getLong("network_id");
-        String auth = json.getString("auth");
-        Network network = networkService.getNetworkById(networkId);
-        Device device = new Device(deviceId, deviceName);
-        List<Device> devices = network.getDevices();
-        int size = devices.size();
-        for (int i = 0; i < size; ++i) {
-            if (device.getId().equals(deviceId)) {
-                devices.remove(i);
-                break;
-            }
-        }
-        //set avg throughput of network..
-        // remove device from db ..	
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
 
 }
